d5d72c50e965a66419c0cbaf81b86217c2763533
chengyue-lin <59622564+chengyue-lin@users.noreply.github.com>
2020-02-28 16:31:47 -0800

    Update RShell.cpp

--- src/RShell.cpp
+++ src/RShell.cpp
@@ -27,3 +27,90 @@ int main()
         parser->execCommand(parser->warpCommand(parser->commandTree(parser->readExpression(parser->readHash(input)))));
     }
 }
+// RShellBase *RParser::warpCommand(queue<char *> container)
+// {
+//     queue<RShellBase *> warpExec;
+//     queue<char *> statment;
+//     stack<char *> ope;
+
+//     while (container.size() != 0)
+//     {
+//         char *tempPtr = strdup(container.front());
+//         if (strchr(tempPtr, '&'))
+//         {
+//             container.pop();
+//             string ret = "";
+//             while (container.size() != 0 && !(container.front()[0] == '&' || container.front()[0] == '|' || container.front()[0] == ';'))
+//             {
+//                 tempPtr = strdup(container.front());
+//                 ret += tempPtr;
+//                 container.pop();
+//                 if (container.size() != 0 && !(container.front()[0] == '&' || container.front()[0] == '|' || container.front()[0] == ';'))
+//                 {
+//                     ret += " ";
+//                 }
+//             }
+//             RShellExec *retExe = new RShellExec(ret.c_str());
+//             RAnd *andExe = new RAnd(warpExec.front(), retExe);
+//             warpExec.pop();
+//             warpExec.push(andExe);
+//         }
+//         else if (strchr(tempPtr, '|'))
+//         {
+//             container.pop();
+//             string ret = "";
+//             while (container.size() != 0 && !(container.front()[0] == '&' || container.front()[0] == '|' || container.front()[0] == ';'))
+//             {
+//                 tempPtr = strdup(container.front());
+//                 ret += tempPtr;
+//                 container.pop();
+//                 if (container.size() != 0 && !(container.front()[0] == '&' || container.front()[0] == '|' || container.front()[0] == ';'))
+//                 {
+//                     ret += " ";
+//                 }
+//             }
+//             RShellExec *retExe = new RShellExec(ret.c_str());
+//             ROr *orExe = new ROr(warpExec.front(), retExe);
+//             warpExec.pop();
+//             warpExec.push(orExe);
+//         }
+//         else if (strchr(tempPtr, ';'))
+//         {
+//             container.pop();
+
+//             string ret = "";
+//             while (container.size() != 0 && !(container.front()[0] == '&' || container.front()[0] == '|' || container.front()[0] == ';'))
+//             {
+//                 tempPtr = strdup(container.front());
+//                 ret += tempPtr;
+//                 container.pop();
+//                 if (container.size() != 0 && !(container.front()[0] == '&' || container.front()[0] == '|' || container.front()[0] == ';'))
+//                 {
+//                     ret += " ";
+//                 }
+//             }
+//             RShellExec *retExe = new RShellExec(ret.c_str());
+//             RSeparator *sepExe = new RSeparator(warpExec.front(), retExe);
+//             warpExec.pop();
+//             warpExec.push(sepExe);
+//         }
+//         else
+//         {
+//             string ret = "";
+//             while (container.size() != 0 && !(container.front()[0] == '&' || container.front()[0] == '|' || container.front()[0] == ';'))
+//             {
+//                 tempPtr = strdup(container.front());
+//                 ret += tempPtr;
+//                 container.pop();
+//                 if (container.size() != 0 && !(container.front()[0] == '&' || container.front()[0] == '|' || container.front()[0] == ';'))
+//                 {
+//                     ret += " ";
+//                 }
+//             }
+//             RShellExec *exe = new RShellExec(ret.c_str());
+//             warpExec.push(exe);
+//         }
+//     }
+
+//     return warpExec.front();
+// }
